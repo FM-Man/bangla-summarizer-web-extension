814c9bacc1a00f63645515853b7eff6c3e205a9b
Fahim Morshed <bsse1102@iit.du.ac.bd>
2023-07-11 14:16:35 +0600

    context menu updated

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+      "title": 'Summarize selected text',
+      "contexts": ["selection"],
+      "id": "myContextMenuId"
   });
+});
   
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  chrome.tabs.create({  
+      url: "http://www.google.com/search?q=" + encodeURIComponent(info.selectionText)
+  });
+})
