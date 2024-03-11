6f205573bb1b6780c07d73dfec584d7b91ab5759
Fahim Morshed <bsse1102@iit.du.ac.bd>
2024-03-11 23:56:29 +0600

    prothom alo bug fixed

--- content_script.js
+++ content_script.js
@@ -1,21 +1,25 @@
 // Function to extract text from elements with specified CSS classes
-function extractText() {
-    var text = "";
-    document.querySelectorAll('.story-element .story-element-text div p').forEach(function (element) {
-        text += element.innerText.trim() + " ";
-    });
-    console.log(text);
-    return text;
-}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.message === 'hello') {
+        isProthomAloArticle();
+        runOnChange();
+    }
+});
+
 
 function runOnChange(){
-
-    if (window.location.href.includes("prothomalo.com") &&
-        window.location.href !== "https://www.prothomalo.com") {
+    var textContent = "";
+    if (isProthomAloArticle()) {
         console.log("url found");
-        var textContent = extractText();
-        // chrome.storage.local.set({ selectedText: textContent });
-        
+        textContent = extractTextFromProthomAlo();
+    }
+    
+
+
+
+    //fetching
+    if (textContent != ""){
         fetch("http://127.0.0.1:8000/summarize", {
             method: 'POST',
             headers: {
@@ -23,83 +27,69 @@ function runOnChange(){
             },
             body: JSON.stringify({ "text": textContent }),
             redirect: 'follow'
-        })
-        .then(response => response.json())
-        .then(data => {
+        }).then(response => response.json()).then(data => {
             // Open popup window and display response string
             const popupWindow = window.open("", "Popup Window", "width=400,height=500,left=940,top=150");
-            popupWindow.document.write(`
-            <!DOCTYPE html>
-            <html lang="en">
-            
-            <head>
-            <meta charset="UTF-8">
-            <title>Bangla Text Summarizer</title>
-            <style>
-                body {
-                font-family: Arial, sans-serif;
-                margin-top: 40;
-                padding: 0;
-                }
-            
-                .container {
-                display: flex;
-                justify-content: space-around;
-                align-items: flex-start;
-                /* Align items to the top */
-                height: fit-content;
-                flex-direction: row;
-                /* Display sections vertically */
-                }
-            
-                .text-section {
-                width: 90%;
-                border: 1px solid #941414;
-                padding: 0px 20px 20px;
-                /* margin-bottom: 20px; */
-                position: relative;
-                }
-            
-            
-                h2 {
-                margin-bottom: 10px;
-                }
-            </style>
-            </head>
-            
-            <body>
-            <div class="container">
-                <!--div class="text-section">
-                <h2>Document</h2>
-                <div id="textSection1">
-                    <p>${data.text}</p>
+            popupWindow.document.write('');
+            popupWindow.document.write(
+            `<html><head>
+                <meta charset="UTF-8">
+                <title>Bangla Text Summarizer</title>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        margin-top: 40;
+                        padding: 0;
+                    }
+                    .container {
+                        display: flex;
+                        justify-content: space-around;
+                        align-items: flex-start;
+                        /* Align items to the top */
+                        height: fit-content;
+                        flex-direction: row;
+                        /* Display sections vertically */
+                    }
+                    .text-section {
+                        width: 90%;
+                        border: 1px solid #941414;
+                        padding: 0px 20px 20px;
+                        /* margin-bottom: 20px; */
+                        position: relative;
+                    }
+                    h2 {margin-bottom: 10px;}
+                </style>
+            </head><body>
+                <div class="container">
+                    <div class="text-section">
+                        <h2>Summary</h2>
+                        <div id="textSection2">
+                            <p>${data.summary}</p>
+                        </div>
+                    </div>
                 </div>
-                </div-->
-                <div class="text-section">
-                <h2>Summary</h2>
-                <div id="textSection2">
-                    <p>${data.summary}</p>
-                </div>
-                </div>
-            </div>
-            
-            
-            </body>
-            
-            </html>`
-            );
-        })
-        .catch(error => console.error('Error:', error));
-
+            </body></html>`);
+        }).catch(error => console.error('Error:', error));
     }
-
 }
 
-console.log("runs##################################");
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.message === 'hello') {
-        console.log("changed##################################");
-        runOnChange();
-    }
-});
 
+//prothom alo
+function isProthomAloArticle(){
+    if(s.includes('prothomalo')){
+        querys = document.querySelectorAll('.story-element .story-element-text div p');
+        if(querys.length !=0)
+            return true;
+        else return false;
+    }
+    else return false;
+}
+
+function extractTextFromProthomAlo() {
+    var text = "";
+    document.querySelectorAll('.story-element .story-element-text div p').forEach(function (element) {
+        text += element.innerText.trim() + " ";
+    });
+    // console.log(text);
+    return text;
+}
